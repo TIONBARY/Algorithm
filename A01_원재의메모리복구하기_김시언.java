c7448052e597b7abfe24dd7ff57e10b5e872fb70
김시언 <rlatldjs123@gmail.com>
2022-02-03 05:02:23 +0000

    Update Solution.java

--- A01_원재의메모리복구하기_김시언.java
+++ A01_원재의메모리복구하기_김시언.java
@@ -1,28 +1,26 @@
-import java.io.File;
 import java.util.Scanner;
+import java.io.FileInputStream;
 import java.util.stream.Stream;
 
-public class Solution {
-
-	public static void main(String[] args) throws Exception{
-		Scanner sc = new Scanner(new File("src/input.txt"));
+class Solution {
+	public static void main(String args[]) throws Exception	{		
+		Scanner sc = new Scanner(System.in);
 		int T, count;
-		T = Integer.parseInt(sc.nextLine());
-		
+		T=sc.nextInt();
+        sc.nextLine();
+
 		for(int test_case = 1; test_case <= T; test_case++) {
 			String str = sc.nextLine();
 			int[] arr = Stream.of(str.split("")).mapToInt(Integer::parseInt).toArray();
 			
-			if(arr[0] == 1) count = 1;
+            if(arr[0] == 1) count = 1;
 			else count = 0;
 			
 			for(int i=0; i<arr.length-1; i++) {
 				if(arr[i] != arr[i+1]) count++;
-			}
-			
-			System.out.println("#" + test_case + " "+ count);
+			}			
+			System.out.println("#" + test_case + " "+ count);	
 		}
-		sc.close();
+        sc.close();
 	}
-
 }
