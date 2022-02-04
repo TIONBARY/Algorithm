dfa4dd2d4c0e7f601eacc5c2fa1becd089e594d6
김시언 <rlatldjs123@gmail.com>
2022-02-04 05:34:55 +0000

    Update Solution.java

--- A04_농작물수확하기/A04_농작물수확하기_김시언.java
+++ A04_농작물수확하기/A04_농작물수확하기_김시언.java
@@ -1,16 +1,13 @@
-package A04_;
-
-import java.io.File;
 import java.util.Scanner;
 
-public class Solution {
-	public static void main(String[] args) throws Exception{
-		Scanner sc = new Scanner(new File("src/A04_/input.txt"));
+class A04_농작물수확하기_김시언{
+	public static void main(String args[]) throws Exception{
+		Scanner sc = new Scanner(System.in);
 		int T,N,width,sum;
 		String temp;
-		T = sc.nextInt();
-		
-		for(int test_case=0; test_case<T; test_case++) {
+		T=sc.nextInt();
+
+		for(int test_case = 1; test_case <= T; test_case++){
 			width = 1;
 			sum = 0;
 			N = sc.nextInt();
@@ -32,8 +29,8 @@ public class Solution {
 					sum += farm[i][j];
 				}
 			}
-			System.out.println("#"+(test_case+1)+" "+sum);
+			System.out.println("#"+test_case+" "+sum);
 		}
-		sc.close();
+        sc.close();
 	}
 }
