ac355f52f87d43477e05544d601d4b4c423a0aa3
허설 <hseol96@naver.com>
2022-02-10 15:01:31 +0000

    Update B06_색종이_허설.java

--- B06_색종이/B06_색종이_허설.java
+++ B06_색종이/B06_색종이_허설.java
@@ -1,36 +1,39 @@
 package algol_1;
 
 import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class B06_색종이_허설 {
- static int[][] map  = new int [100][100]; 
- 	
-public static void main(String[] args) throws Exception{
+	static int[][] map = new int[100][100];// 0,0도 있으니까
+
+	public static void main(String[] args) throws IOException {
+
 		//BufferedReader br = new BufferedReader(new FileReader("src\\algol_1\\input.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
-		int N = Integer.parseInt(br.readLine()); //사각형의 개수 
-        int count; 
-		while(N-- >0) {
+
+		int N = Integer.parseInt(br.readLine()); // 사각형의 개수
+		int count = 0;
+		while (N-- > 0) {
 			st = new StringTokenizer(br.readLine());
 			int curX = Integer.parseInt(st.nextToken()); 
-			int curY = Integer.parseInt(st.nextToken());
-
-			for(int i=curX;i<curX+10;i++) {
-				for(int j= curY;j<curY+10;j++) {
-					map[i][j]=1;
+			int curY = Integer.parseInt(st.nextToken()); 
+			for (int i = curX; i < curX + 10; i++) {
+				for (int j = curY; j < curY + 10; j++) {
+					map[i][j] = 1;
 				}
 			}
-		}//////////////////////while문 종료 
-		for(int i=0;i<100;i++) {
-			for(int j=0;j<100;j++) {
-				if(map[i][j]==1)
+
+		}
+		for (int i = 0; i < 100; i++) {
+			for (int j = 0; j < 100; j++) {
+				if (map[i][j] == 1)
 					count++;
 			}
 		}
-		System.out.println(count); 
-    }//////////////////main
+		System.out.println(count);
+	}
 
 }
