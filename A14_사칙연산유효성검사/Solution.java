81a1692f8fe4cb2b1cdb90aacde9dd934c47fbcd
김시언 <rlatldjs123@gmail.com>
2022-02-10 08:14:07 +0000

    Upload New File

---
+++ A14_사칙연산유효성검사/Solution.java
@@ -0,0 +1,51 @@
+package A14_사칙연산유효성검사;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("src/A14_사칙연산유효성검사/input.txt"));
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		char operator;
+		int N, result;
+		for(int i=1; i<=10; i++) {
+			N = Integer.parseInt(br.readLine());
+			result = 1;
+			
+			for(int j=1; j<=N; j++) {
+				st = new StringTokenizer(br.readLine());
+				st.nextToken();
+				
+				operator = st.nextToken().charAt(0);
+				if(operator == '+' || operator == '-' || operator == '*' || operator == '/') {
+					if(!st.hasMoreTokens()) {
+						result = 0;
+						continue;
+					}
+					else {
+						st.nextToken();
+						if(!st.hasMoreTokens()) {
+							result = 0;
+							continue;
+						}
+					}
+				}
+				else {
+					if(st.hasMoreTokens()) {
+						result = 0;
+						continue;
+					}
+				}
+			}
+			
+			System.out.println("#"+i+" "+result);
+		}
+		
+		br.close();
+	}
+}
