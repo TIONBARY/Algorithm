cea73cfb3cab86e0f31cb5b216115c3223d6f37e
김시언 <rlatldjs123@gmail.com>
2022-02-10 08:14:38 +0000

    Update Solution.java

--- A14_사칙연산유효성검사/A14_사칙연산유효성검사_김시언.java
+++ A14_사칙연산유효성검사/A14_사칙연산유효성검사_김시언.java
@@ -1,15 +1,11 @@
-package A14_사칙연산유효성검사;
-
 import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Solution {
+public class A14_사칙연산유효성검사_김시언 {
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new FileReader("src/A14_사칙연산유효성검사/input.txt"));
-		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st;
 		char operator;
 		int N, result;
