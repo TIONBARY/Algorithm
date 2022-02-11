cb99f658ec099f35f350bd9764d772c8102a04d5
김시언 <rlatldjs123@gmail.com>
2022-02-11 07:51:50 +0000

    Upload New File

---
+++ A실습/A18_Compare/Student.java
@@ -0,0 +1,12 @@
+package Compare;
+
+public class Student {
+	int age, point;
+	String name;
+	
+	public Student(int age, int point, String name) {
+		this.age = age;
+		this.point = point;
+		this.name = name;
+	}
+}
