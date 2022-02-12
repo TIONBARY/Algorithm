6417482d811ca8ab28c2b48c2d97b768dfc12e66
허설 <hseol96@naver.com>
2022-02-12 16:35:49 +0000

    Update C03_게임_허설.java

--- C숙제/C03_게임/C03_게임_허설.java
+++ C숙제/C03_게임/C03_게임_허설.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class C03_야바위 { 
+public class C03_게임_허설 { 
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
