6e6347bb0a2b7c5f55e38a48df0999e83a0ad6e2
김시언 <rlatldjs123@gmail.com>
2022-02-13 07:18:22 +0000

    Upload New File

---
+++ C숙제/C03_게임/Main.java
@@ -0,0 +1,35 @@
+package C03_게임;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new FileReader("src/C03_게임/input.txt"));
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N, X, K, ball, from, to;
+		
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken());
+		X = Integer.parseInt(st.nextToken());
+		K = Integer.parseInt(st.nextToken());
+		ball = X;
+		
+		for(int i=0; i<K; i++) {
+			st = new StringTokenizer(br.readLine());
+			from = Integer.parseInt(st.nextToken());
+			to = Integer.parseInt(st.nextToken());
+			
+			if(from == ball) ball = to;
+			else if(to == ball) ball = from;
+			else continue;
+		}
+		
+		System.out.println(ball);
+		br.close();
+	}
+}
