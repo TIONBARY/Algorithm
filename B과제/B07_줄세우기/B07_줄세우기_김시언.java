44d21c948bb424d22c8b40101cd9a64757d31054
김시언 <rlatldjs123@gmail.com>
2022-02-15 13:03:48 +0000

    Update Main.java

--- B과제/B07_줄세우기/B07_줄세우기_김시언.java
+++ B과제/B07_줄세우기/B07_줄세우기_김시언.java
@@ -1,12 +1,10 @@
-package B07_줄세우기;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
-public class Main {
+public class B07_줄세우기_김시언 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		LinkedList<Integer> list = new LinkedList<Integer>();
