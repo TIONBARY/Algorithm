15572ca93655d6dbb75deab6216a20348daceeb1
김시언 <rlatldjs123@gmail.com>
2022-02-17 00:39:08 +0000

    Update B09_쿼드트리_김시언.java

--- B과제/B09_쿼드트리/B09_쿼드트리_김시언.java
+++ B과제/B09_쿼드트리/B09_쿼드트리_김시언.java
@@ -6,7 +6,8 @@ public class B09_쿼드트리_김시언 {
 	static int map[][];
 	static StringBuilder sb = new StringBuilder();
 	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new FileReader("src/B09_쿼드트리/input.txt"));
 		String s;
 		int N;
 		N = Integer.parseInt(br.readLine());
@@ -24,33 +25,26 @@ public class B09_쿼드트리_김시언 {
 	}
 	
 	public static void mapping(int x, int y, int n) {
-		int size = n/2;
-		
-		if(check(x, y, n)) {
-			sb.append(map[x][y]);
-			return;
-		}
-		
-		sb.append("(");
-		
-		mapping(x, y, size);
-		mapping(x, y+size, size);
-		mapping(x+size, y, size);
-		mapping(x+size, y+size, size);
-		
-		sb.append(")");
-	}
-	
-	public static boolean check(int x, int y, int n) {
-		int point = map[x][y];
+		int sum = 0;
 		
 		for(int i=x; i<x+n; i++) {
 			for(int j=y; j<y+n; j++) {
-				if(point != map[i][j]) {
-					return false;
-				}
+				sum += map[i][j];
 			}
 		}
-		return true;
+		
+		if(sum == n*n) sb.append(1);
+		else if(sum == 0) sb.append(0);
+		else {
+			int size = n/2;
+			sb.append("(");
+			
+			mapping(x, y, size);
+			mapping(x, y+size, size);
+			mapping(x+size, y, size);
+			mapping(x+size, y+size, size);
+			
+			sb.append(")");
+		}
 	}
 }
